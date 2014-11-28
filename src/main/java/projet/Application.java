b30cd0065dc04571c668ce651c105341010286d6
nour.belhadj-ahmed <nou690@hotmail.fr>
2014-11-28 16:22:12 +0100

    maj encore

--- src/main/java/projet/Application.java
+++ src/main/java/projet/Application.java
@@ -40,47 +40,47 @@ public class Application {
         ClientRepository crepository = context.getBean(ClientRepository.class);
         CodePromoRepository cprepository = context.getBean(CodePromoRepository.class);
         
-        prepository.save(new Produit("Orange", "Nourriture", 10.0 , "orange" , 1000));
-        prepository.save(new Produit("Pomme", "Nourriture", 10.0 , "orange" , 1000));
-        prepository.save(new Produit("Poire", "Nourriture", 10.0 , "orange" , 1000)); 
-        prepository.save(new Produit("Avocat", "Nourriture", 10.0 , "orange" , 1000));
-        prepository.save(new Produit("Prune", "Nourriture", 10.0 , "orange" , 1000));
-        prepository.save(new Produit("Fraise", "Nourriture", 10.0 , "orange" , 1000));
-        prepository.save(new Produit("Figue", "Nourriture", 10.0 , "orange" , 1000));  
-        prepository.save(new Produit("Citron", "Nourriture", 10.0 , "orange" , 1000));
-        prepository.save(new Produit("Raisin", "Nourriture", 10.0 , "orange" , 1000));
-        prepository.save(new Produit("Framboise", "Nourriture", 10.0 , "orange" , 1000));
-        prepository.save(new Produit("kiwi", "Nourriture", 10.0 , "orange" , 1000));
-        prepository.save(new Produit("Banane", "Nourriture", 95.0 , "banane" , 1000));
-        prepository.save(new Produit("abricot", "Nourriture", 10.0 , "orange" , 1000));
-        prepository.save(new Produit("peche", "Nourriture", 10.0 , "orange" , 1000));
-        prepository.save(new Produit("pamplemousse", "Nourriture", 10.0 , "orange" , 1000));
+        prepository.save(new Produit("Orange", "Nourriture", 1.0 , "orange" , 1000));
+        prepository.save(new Produit("Pomme", "Nourriture", 1.0 , "orange" , 1000));
+        prepository.save(new Produit("Poire", "Nourriture", 1.0 , "orange" , 1000)); 
+        prepository.save(new Produit("Avocat", "Nourriture", 1.0 , "orange" , 1000));
+        prepository.save(new Produit("Prune", "Nourriture", 1.0 , "orange" , 1000));
+        prepository.save(new Produit("Fraise", "Nourriture", 1.0 , "orange" , 1000));
+        prepository.save(new Produit("Figue", "Nourriture", 1.0 , "orange" , 1000));  
+        prepository.save(new Produit("Citron", "Nourriture", 1.0 , "orange" , 1000));
+        prepository.save(new Produit("Raisin", "Nourriture", 1.0 , "orange" , 1000));
+        prepository.save(new Produit("Framboise", "Nourriture", 1.0 , "orange" , 1000));
+        prepository.save(new Produit("kiwi", "Nourriture", 1.0 , "orange" , 1000));
+        prepository.save(new Produit("Banane", "Nourriture", 1.0 , "banane" , 1000));
+        prepository.save(new Produit("abricot", "Nourriture", 1.0 , "orange" , 1000));
+        prepository.save(new Produit("peche", "Nourriture", 1.0 , "orange" , 1000));
+        prepository.save(new Produit("pamplemousse", "Nourriture", 1.0 , "orange" , 1000));
         
         
-        prepository.save(new Produit("armani pulls homme", "Vetements", 10.0 , "armani pulls homme" , 1000));
-        prepository.save(new Produit("jeans femme bleu", "Vetements", 10.0 , "jeans femme bleu" , 1000));
-        prepository.save(new Produit("jeans homme bleu", "Vetements", 10.0 , "jeans homme bleu" , 1000)); 
-        prepository.save(new Produit("pull en angora rouge sud", "Vetements", 10.0 , "pull en angora rouge sud" , 1000));
-        prepository.save(new Produit("pull en maille", "Vetements", 10.0 , "pull en maille" , 1000));
-        prepository.save(new Produit("pull capuche maille", "Vetements", 10.0 , "pull capuche maille" , 1000));  
-        prepository.save(new Produit("ralph lauren pulls vert", "Vetements", 10.0 , "ralph lauren pulls vert" , 1000));
+        prepository.save(new Produit("armani pulls homme", "Vetements", 25.0 , "armani pulls homme" , 1000));
+        prepository.save(new Produit("jeans femme bleu", "Vetements", 25.0 , "jeans femme bleu" , 1000));
+        prepository.save(new Produit("jeans homme bleu", "Vetements", 25.0 , "jeans homme bleu" , 1000)); 
+        prepository.save(new Produit("pull en angora rouge sud", "Vetements", 25.0 , "pull en angora rouge sud" , 1000));
+        prepository.save(new Produit("pull en maille", "Vetements", 25.0 , "pull en maille" , 1000));
+        prepository.save(new Produit("pull capuche maille", "Vetements", 25.0 , "pull capuche maille" , 1000));  
+        prepository.save(new Produit("ralph lauren pulls vert", "Vetements", 25.0 , "ralph lauren pulls vert" , 1000));
         
         
-        prepository.save(new Produit("coca 0.5l", "Boisson", 10.0 , "coca 0.5l" , 1000));
-        prepository.save(new Produit("coca 33cl", "Boisson", 10.0 , "coca 33cl" , 1000));
-        prepository.save(new Produit("fanta 33cl", "Boisson", 10.0 , "fanta 33cl" , 1000));
-        prepository.save(new Produit("orangina", "Boisson", 95.0 , "orangina" , 1000));
+        prepository.save(new Produit("coca 0.5l", "Boisson", 1.0 , "coca 0.5l" , 1000));
+        prepository.save(new Produit("coca 33cl", "Boisson", 1.0 , "coca 33cl" , 1000));
+        prepository.save(new Produit("fanta 33cl", "Boisson", 1.0 , "fanta 33cl" , 1000));
+        prepository.save(new Produit("orangina", "Boisson",1.0 , "orangina" , 1000));
         
         
-        prepository.save(new Produit("tele sony", "Autre", 10.0 , "tele sony" , 1000));
-        prepository.save(new Produit("tele samsung 3d", "Autre", 10.0 , "tele samsung 3d" , 1000));
+        prepository.save(new Produit("tele sony", "Autre", 700.0 , "tele sony" , 1000));
+        prepository.save(new Produit("tele samsung 3d", "Autre", 1000.0 , "tele samsung 3d" , 1000));
         
         
-        prepository.save(new Produit("machine a laver bosch", "Electroménager", 10.0 , "machine a laver bosch" , 1000));
-        prepository.save(new Produit("machine a laver samsung", "Electroménager", 10.0 , "machine a laver samsung" , 1000));
-        prepository.save(new Produit("micro onde whirlpool", "Electroménager", 10.0 , "micro onde whirlpool" , 1000)); 
-        prepository.save(new Produit("mixeur", "Electroménager", 10.0 , "mixeur" , 1000));
-        prepository.save(new Produit("frigo americain", "Electroménager", 10.0 , "frigo americain" , 1000));
+        prepository.save(new Produit("machine a laver bosch", "Electroménager", 450.0 , "machine a laver bosch" , 1000));
+        prepository.save(new Produit("machine a laver samsung", "Electroménager", 450.0 , "machine a laver samsung" , 1000));
+        prepository.save(new Produit("micro onde whirlpool", "Electroménager", 100.0 , "micro onde whirlpool" , 1000)); 
+        prepository.save(new Produit("mixeur", "Electroménager", 70.0 , "mixeur" , 1000));
+        prepository.save(new Produit("frigo americain", "Electroménager", 300.0 , "frigo americain" , 1000));
         
 
         crepository.save(new Client("jean","pdg", "test" , "n@gmail.com", "test"));
